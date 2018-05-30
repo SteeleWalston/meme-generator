50b6fdf6676d27d86da51cf22ae360e910ba3bd4
Steele Walston <steele.walston@gmail.com>
2018-05-29 17:21:09 -0700

    about to add default image and image loader

--- src/components/App.js
+++ src/components/App.js
@@ -20,6 +20,44 @@ export default class App extends Component {
         return (
             <main>
                 <h2>Cool Stuff</h2>
+                <fieldset>
+                    <div>
+                        <label>
+                            Header Text:
+                            <input 
+                                value={headerText}
+                            />
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Footer Text:
+                            <input 
+                                value={footerText}
+                            />
+                        </label>
+                    </div>
+                </fieldset>
+
+                <section>
+                    <div>
+                        <label>
+                            Image Src:
+                            <input/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Image:
+                            <input
+                                type="file"
+                            />
+                        </label>
+                    </div>
+                    <div className="image-container">
+                        <img src={image}/>
+                    </div>
+                </section>
             </main>
         );
     }
